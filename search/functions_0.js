171719ccd8ec74467ae0f74bafb3360d41e3a48d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-22 07:42:52 +0000

    Deploy John-Jasper-Doe/Lab5 to github.com/John-Jasper-Doe/Lab5.git:gh-pages

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../classsparse__matrix.html#ab32292bb4a65a3e49380f19e2c1dd6b2',1,'sparse_matrix']]]
+  ['begin',['begin',['../classsparse__matrix.html#ab32292bb4a65a3e49380f19e2c1dd6b2',1,'sparse_matrix::begin()'],['../classsparse__matrix.html#a6e2a93817d5f2c2ffd4593a7b3f78ca4',1,'sparse_matrix::begin() const ']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['end',['end',['../classsparse__matrix.html#a27ebdaaeb82be8abf868fdaa10a6ecc1',1,'sparse_matrix']]]
+  ['end',['end',['../classsparse__matrix.html#a27ebdaaeb82be8abf868fdaa10a6ecc1',1,'sparse_matrix::end()'],['../classsparse__matrix.html#a5e3f464c541400f7fd3f621fc6513546',1,'sparse_matrix::end() const ']]]
 ];
